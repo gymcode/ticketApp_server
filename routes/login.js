1958ce6c5bf318cb35568c0124deb3e2768f46d7
gymcode <kyleabs20@gmail.com>
2020-05-08 16:11:31 -0600

    login section

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const User = require('../models/user.model');
+const { User } = require('../models/user.model');
 
 
 router.post('/', async(req, res)=>{
